fb31e499fdd33ad69950b92f02904acd03cc4bf5
Francis Tin-ao <franfra10j@gmail.com>
2024-12-09 11:30:31 +0800

    feat: :sparkles: added basic create functions for farmer and land

--- app/Http/Controllers/Api/FarmerController.php
+++ app/Http/Controllers/Api/FarmerController.php
@@ -12,5 +12,38 @@ class FarmerController extends Controller
     //
     public function create_farmer(Request $request)
     {
+        try {
+            $request->validate([
+                'name' => 'required|string',
+                'gender' => 'required|string',
+                'number_of_coconut_trees' => 'required|integer',
+                'barangay_id' => 'required|integer',
+            ]);
+
+            $farmer = new Farmer();
+            $farmer->name = $request->name;
+            $farmer->gender = $request->gender;
+            $farmer->number_of_coconut_trees =
+                $request->number_of_coconut_trees;
+            $farmer->barangay_id = $request->barangay_id;
+
+            $farmer->save();
+            $farmer_id = $farmer->farmer_id;
+
+            return response()->json(
+                [
+                    'farmer_id' => $farmer_id,
+                    'message' => 'Farmer created successfully',
+                ],
+                201
+            );
+        } catch (\Exception $e) {
+            return response()->json(
+                [
+                    'message' => 'Error: ' . $e->getMessage(),
+                ],
+                500
+            );
+        }
     }
 }
--- app/Http/Controllers/Api/LandController.php
+++ app/Http/Controllers/Api/LandController.php
@@ -5,7 +5,42 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
+use App\Models\Land;
+
 class LandController extends Controller
 {
     //
+    public function create_land(Request $request)
+    {
+        try {
+            $request->validate([
+                'is_affected' => 'required|boolean',
+                'land_location' => 'required|string',
+                'farmer_id' => 'required|integer',
+                'barangay_id' => 'required|integer',
+            ]);
+
+            $land = new Land();
+            $land->is_affected = $request->is_affected;
+            $land->land_location = $request->land_location;
+            $land->farmer_id = $request->farmer_id;
+            $land->barangay_id = $request->barangay_id;
+
+            $land->save();
+
+            return response()->json(
+                [
+                    'message' => 'Land created successfully',
+                ],
+                201
+            );
+        } catch (\Exception $e) {
+            return response()->json(
+                [
+                    'message' => 'Error: ' . $e->getMessage(),
+                ],
+                500
+            );
+        }
+    }
 }
