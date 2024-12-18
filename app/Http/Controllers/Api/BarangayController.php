3602e13b2b88c4173f852b8a783d490fcc2d0e83
Francis Tin-ao <franfra10j@gmail.com>
2024-12-18 18:27:43 +0800

    feat: :alien: added few key and values to be included in the response

--- app/Http/Controllers/Api/BarangayController.php
+++ app/Http/Controllers/Api/BarangayController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 use App\Models\Barangay;
+use App\Models\Land;
+use App\Models\Farmer;
 
 class BarangayController extends Controller
 {
@@ -14,6 +16,21 @@ class BarangayController extends Controller
     {
         //return all barangay and count the # of farmers and lands
         $barangays = Barangay::withCount(['farmers', 'lands'])->get();
+
+        //get the lands of each barangay from the lands table
+        foreach ($barangays as $barangay) {
+            $lands = Land::where('barangay_id', $barangay->barangay_id)->get([
+                'is_affected',
+                'land_location',
+            ]);
+            $coconut_trees_planted = Farmer::where(
+                'barangay_id',
+                $barangay->barangay_id
+            )->sum('number_of_coconut_trees');
+            $barangay->lands = $lands;
+            $barangay->coconut_trees_planted = $coconut_trees_planted;
+        }
+
         return response()->json($barangays);
     }
 
