0741394c053e2d3b119fde68630f88a3b9702f54
Francis Tin-ao <franfra10j@gmail.com>
2024-12-23 17:10:00 +0800

    fix: :ambulance: added installation of autoload in pre-deployment script

--- scripts/00-laravel-deploy.sh
+++ scripts/00-laravel-deploy.sh
@@ -1,10 +1,23 @@
 #!/usr/bin/env bash
-echo "Running composer"
-composer global require hirak/prestissimo
-composer install --no-dev --working-dir=/var/www/html
-echo "Caching config..."
+
+echo "Setting up Laravel application..."
+
+php -v
+
+echo "Installing composer dependencies..."
+composer install --optimize-autoloader --no-dev
+
+echo "Clearing caches..."
+php artisan cache:clear
+php artisan config:clear
+php artisan route:clear
+php artisan view:clear
+
+echo "Caching configurations..."
 php artisan config:cache
-echo "Caching routes..."
 php artisan route:cache
+
 echo "Running migrations..."
 php artisan migrate --force
+
+echo "Deployment complete! Let's fucking go!"
